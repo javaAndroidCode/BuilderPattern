8e57c81f6c7694bfd1a29aaecf9145068542aed1
javaAndroidCode <javaandroidcode@gmail.com>
2013-09-16 13:42:39 +0300

    Update Client.java

--- src/main/Client.java
+++ src/main/Client.java
@@ -1,12 +1,6 @@
 package main;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Пользователь
- * Date: 16.09.13
- * Time: 11:09
- * To change this template use File | Settings | File Templates.
- */
+
 public class Client {
 
     public static void main(String[] args) {
